c352d005b8e1f5c188bc4e15581feb9c6906eb9e
The-Pain-01 <rubangomoses000@gmail.com>
2026-08-04 15:38:09 +0200

    Create password.js

---
+++ password.js
@@ -0,0 +1,23 @@
+/* ==================================
+   THE PAIN SUPPORT PASSWORD
+   ================================== */
+
+
+
+const SITE_PASSWORD = "THEPAIN2026";
+
+
+
+/*
+
+============================
+
+CHANGE YOUR PASSWORD HERE
+
+Example:
+
+const SITE_PASSWORD = "MYNEWPASSWORD";
+
+============================
+
+*/
